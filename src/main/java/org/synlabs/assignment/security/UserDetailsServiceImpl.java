7d8b37ddaf0ce8299845c409a4f96eb9888491cc
Nikkk28 <marthanikshith.28@gmail.com>
2025-10-25 01:22:00 +0530

    security setup

---
+++ src/main/java/org/synlabs/assignment/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package org.synlabs.assignment.security;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.synlabs.assignment.model.User;
+import org.synlabs.assignment.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email)
+                );
+
+        return UserDetailsImpl.build(user);
+    }
+}
