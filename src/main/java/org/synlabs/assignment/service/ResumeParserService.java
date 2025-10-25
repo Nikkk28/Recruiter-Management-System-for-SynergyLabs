4fbbd3d9f5cacfda7287b4a4c3843cfec7faa511
Nikkk28 <marthanikshith.28@gmail.com>
2025-10-25 09:10:13 +0530

    final

--- src/main/java/org/synlabs/assignment/service/ResumeParserService.java
+++ src/main/java/org/synlabs/assignment/service/ResumeParserService.java
@@ -1,6 +1,6 @@
 package org.synlabs.assignment.service;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
